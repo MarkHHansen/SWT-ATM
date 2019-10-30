dadaa221ba94a84b4fcb475047b352444e8d61f2
maxxum12@gmail.com <maxxum12@gmail.com>
2019-10-30 13:13:55 +0100

    Update for outputfilter

---
+++ ATM/ATM/Outputvalidation_/LogSeperationEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Converter;
+
+namespace ATM.OutputValidation
+{
+    public class LogSeperationEventArgs : EventArgs
+    {
+        public LogSeperationEventArgs(List<Airplane> ap)
+        {
+
+
+        }
+
+
+        public List<Airplane> planesToLog
+        {
+            get;
+            set;
+        }
+
+    }
+}
