cf5b4968a9f1a38f95cd0719323c31dfbf30c1fb
Mathias Tørnes Pedersen <au591121@uni.au.dk>
2019-10-30 13:54:24 +0100

    Logger opdatering

--- ATM/ATM/Logger/LogFile.cs
+++ ATM/ATM/Logger/LogFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace AirTrafficMonitor.Logger
     {
         LogFile()
         {
-
+            MakeFolder();
         }
         public void Print(List<string> airplanes)
         {
