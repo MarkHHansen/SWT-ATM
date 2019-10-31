3be94f41f5278073164109301430ec9dbae048bb
mads6527 <mads6527@hotmail.com>
2019-10-31 15:16:11 +0100

    Velocity and CompassCourse test working

---
+++ ATM/ATM.Unit.Test/CompassCourseUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+using ATM.Converter;
+
+
+namespace ATM.Unit.Test
+{
+    [TestFixture]
+    public class CompassCourseUnitTest
+    {
+
+        private CompasCourse _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            //arrange
+            _uut = new CompasCourse();
+        }
+
+        [Test]
+        public void Test_CalculateCompassCourse()
+        {
+            //act
+            double Course = _uut.CalculateCompassCourse(2, 4, 6, 10);
+
+            //assert
+            Assert.That(Course, Is.EqualTo(56.309932474020213086));
+        }
+
+    }
+}
