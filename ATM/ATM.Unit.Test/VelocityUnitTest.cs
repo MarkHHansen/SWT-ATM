cc06db497b08ba8ceb77dc81ffd0b8a636d6cf33
mads6527 <mads6527@hotmail.com>
2019-10-31 13:30:59 +0100

    Added VelocityTest class

---
+++ ATM/ATM.Unit.Test/VelocityUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+using ATM.Converter;
+
+namespace ATM.Unit.Test
+{
+    [TestClass]
+    public class VelocityUnitTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+    }
+}
