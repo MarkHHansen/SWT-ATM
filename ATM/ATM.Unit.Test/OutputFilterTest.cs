1842792b71dfa640ebc689d5def6b5303d9d0d73
Mathias Tørnes Pedersen <au591121@uni.au.dk>
2019-10-31 15:51:51 +0100

    test

--- ATM/ATM.Unit.Test/OutputFilterTest.cs
+++ ATM/ATM.Unit.Test/OutputFilterTest.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using ATM.Converter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 using NUnit.Framework;
 using TransponderReceiver;
 using ATM.OutputValidation_;
+using ATM.ValidateAirplane;
+using ATM.Converter;
 
 namespace ATM.Unit.Test
 {
     
     class OutputFilterTest
     {
-        private OutputFilter _uut;
-        
+        private AirplaneValidation _uut;
+        private ValidationEventArgs _recivedplaneargs;
+        //private IAirplaneValidation _Iplane;
+
+        //[SetUp]
+        //public void Setup()
+        //{
+        //    _recivedplaneargs = null;
+
+        //    _uut = new AirplaneValidation(_recivedplaneargs);
+        //   // _uut.CurrentAirplanes
+
+        //   _uut.ValidationEvent +=
+                
+
+
+        }
+       
+
+
     }
 }
