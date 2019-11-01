5b4fc0f141c8aa093211478627aa219b2430d7a9
maxxum12@gmail.com <maxxum12@gmail.com>
2019-11-01 10:36:45 +0100

    LogFileTest update

--- ATM/ATM/Logger/IConLogger.cs
+++ ATM/ATM/Logger/IConLogger.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM.Converter;
 
 namespace ATM.Logger
 {
-    public interface ILogger
+    public interface IConLogger
     {
-        void LogCollision(List<string> messages);
+        void PrintAirplanes(List<Airplane> airplanes);
     }
 }
